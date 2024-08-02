8f5c7b08768facbbfeddbe89f179e957a4aa96ed
Roman Golovko <rgolovkl6@gmail.com>
2024-08-02 18:37:32 +0300

    init: start human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function toReadable (number) {
-  
+    let arr0_9 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let arr10_20 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+    let arr30_90 = ['', '', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    let a100 = 'hundred';
 }
